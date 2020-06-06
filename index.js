a4ac92449f3d0b8601ab55f7a723bf3b81217520
John Schreier <jschreier18@gmail.com>
2020-06-06 18:48:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function whisper (string) {
   return string.toLowerCase ()
 }
 function logShout (string) {
-  console.log (HELLO)
+  console.log (shout)
 }
 function logWhisper (string) {
-  console.log (hello)
+  console.log (whisper)
 }
